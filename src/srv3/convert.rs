6bc1b25e62c2cd213809f829b5a896dcca7d6ac1
Hubert Głuchowski <fishhh@fishhh.dev>
2024-12-22 15:18:03 +0100

    Properly set font names when converting SRV3

--- src/srv3/convert.rs
+++ src/srv3/convert.rs
@@ -4,6 +4,20 @@ use super::Document;
 
 const BASE_FONT_SIZE: u32 = 38;
 
+const SRV3_FONTS: &[&str] = &[
+    "Roboto", // also the default
+    "Courier New",
+    "Times New Roman",
+    "Lucida Console",
+    "Comis Sans Ms",
+    "Monotype Corsiva",
+    "Carrois Gothic Sc",
+];
+
+fn font_style_to_name(style: u32) -> &'static str {
+    SRV3_FONTS.get(style as usize).map_or(SRV3_FONTS[0], |v| v)
+}
+
 fn font_size_to_pt(size: u16) -> f32 {
     BASE_FONT_SIZE as f32 * (1.0 + ((size as f32 / 100.0) - 1.0) / 4.0)
 }
@@ -20,7 +34,7 @@ pub fn convert(document: Document) -> Subtitles {
 
         for segment in event.segments.iter() {
             segments.push(crate::Segment::Text(crate::TextSegment {
-                font: "DejaVu Sans".to_string(),
+                font: font_style_to_name(segment.pen().font_style).to_owned(),
                 font_size: font_size_to_pt(segment.pen().font_size),
                 font_weight: if segment.pen().bold { 700 } else { 400 },
                 italic: false,

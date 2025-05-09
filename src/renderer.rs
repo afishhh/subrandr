34bc2d9dfb648112fbd3bf9c56dedcdad7566077
Hubert Głuchowski <fishhh@fishhh.dev>
2025-05-10 01:47:14 +0200

    Add C API for Vulkan wgpu rasterizer

--- src/renderer.rs
+++ src/renderer.rs
@@ -533,7 +533,7 @@ impl Renderer<'_> {
         Ok(())
     }
 
-    fn render_to(
+    pub(crate) fn render_to(
         &mut self,
         rasterizer: &mut dyn Rasterizer,
         target: &mut RenderTarget,

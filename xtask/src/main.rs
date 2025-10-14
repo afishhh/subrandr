5d86eb7752cd3f611f3751c3e10067f4ade47766
Hubert Głuchowski <fishhh@fishhh.dev>
2025-10-14 16:25:56 +0200

    i686 is all you need

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -236,12 +236,6 @@ Libs.private: {required_static_libs}
     ))
 }
 
-fn target_has_broken_implib(target: &Triple) -> bool {
-    target.is_windows()
-        && target.env.as_deref().is_some_and(|env| env != "msvc")
-        && matches!(&*target.arch, "i686")
-}
-
 fn write_implib(
     target: &Triple,
     def_content: &str,
@@ -385,17 +379,6 @@ fn main() -> Result<()> {
                 .destdir
                 .or_else(|| std::env::var_os("DESTDIR").map(PathBuf::from))
                 .unwrap_or_else(|| prefix.clone());
-
-            if target_has_broken_implib(&install.build.target) {
-                eprintln!(
-                    "\x1b[1;31mERROR\x1b[0m: Building for {} is currently known to be broken!",
-                    install.build.target
-                );
-                eprintln!(
-                    "\x1b[1;31mERROR\x1b[0m: See issue #31 at https://github.com/afishhh/subrandr/issues/31"
-                );
-                panic!("Build for broken platform aborted");
-            }
 
             build_library(&manifest_dir, &install.build, args.quiet)?;
 

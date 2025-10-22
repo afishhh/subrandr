7295a94e18fb9951bcbd3c11b3f0020e0f59c3b9
Hubert Głuchowski <fishhh@fishhh.dev>
2025-10-22 19:37:21 +0200

    wip: layout_tests

--- sbr-rasterize/src/color.rs
+++ sbr-rasterize/src/color.rs
@@ -30,6 +30,7 @@ impl BGRA8 {
     pub const ZERO: Self = Self::new(0, 0, 0, 0);
 
     pub const ORANGERED: Self = Self::new(0xFF, 0x45, 0x00, 255);
+    pub const LIGHTSKYBLUE: Self = Self::new(0x87, 0xCE, 0xFA, 255);
 
     pub const fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
         Self { r, g, b, a }
